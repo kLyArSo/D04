77ec986036f86da47ca543eaef8834855b66771b
Yahia L'houari <ylhouari@e1r8p4.1337.ma>
2021-10-31 14:44:03 +0100

    HoHo

---
+++ ex00/Dog.hpp
@@ -0,0 +1,20 @@
+#ifndef _DOG_HPP
+#define _DOG_HPP
+
+#include "Animal.hpp"
+
+class Dog : public Animal
+{
+    private:
+        ;
+    public:
+        Dog();
+        Dog(const Dog &);
+        void    operator=(const Dog &);
+        ~Dog();
+
+        void    makeSound() const;
+};
+
+
+#endif
